7c1e7a67f1649bf36f6ca75bdb8c5ba961c8c345
Harsh Kumar Shrestha <harsh.work4365@gmail.com>
2026-09-06 01:00:36 +0530

    Time: 35 ms (32.00%) | Memory: 12.4 MB (72.00%) - LeetSync

---
+++ 1197-parsing-a-boolean-expression/parsing-a-boolean-expression.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def parseBoolExpr(self, expression):
+        """
+        :type expression: str
+        :rtype: bool
+        """
+        def evaluate(op , arr):
+            bool_vals = [True if x=='t' else False for x in arr]
+
+            if(op=='!'):
+                res = not bool_vals[0]
+            elif op=='&':
+                res = all(bool_vals)
+            elif op=='|':
+                res = any(bool_vals)
+            
+            return 't' if res else 'f'
+
+        stk = []
+
+        for ch in expression :
+            if ch==',' : 
+                continue
+            elif ch==')' : # )
+                temp = []
+                while(len(stk)!=0 and stk[-1] != '('):
+                    temp.append(stk.pop())
+                stk.pop() # remove top ')' 
+                op = stk.pop() # our operation
+                evaluation = evaluate(op,temp)
+                stk.append(evaluation)
+            else: # t f ! & | (
+                stk.append(ch)
+        print(stk[-1])
+        return stk[-1]=='t'
+
+                    
+
+        
